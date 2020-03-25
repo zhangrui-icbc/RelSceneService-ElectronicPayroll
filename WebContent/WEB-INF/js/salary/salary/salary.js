509a3bd360baf6a2cb7c2d8291c62132a8b02f0c
tuwosh <434716122@qq.com>
2020-03-25 16:19:00 +0800

    优化点击清除后新加入的三个汇总字段消息的问题

--- WebContent/WEB-INF/js/salary/salary/salary.js
+++ WebContent/WEB-INF/js/salary/salary/salary.js
@@ -652,6 +652,9 @@ $(".clear").click(function(){
 	html1+="<span data-id='1' data-category='11' data-type='实际收入' data-name='实际收入' >实际收入</span>";
 	html1+="<span data-id='2' data-category='22' data-type='收入合计' data-name='收入合计' >收入合计</span>";
 	html1+="<span data-id='3' data-category='33' data-type='支出合计' data-name='支出合计' >支出合计</span>";
+	html1+="<span data-id='4' data-category='44' data-type='专项附加扣除' data-name='专项附加扣除' >专项附加扣除</span>";
+	html1+="<span data-id='5' data-category='55' data-type='单位支出' data-name='单位支出' >单位支出</span>";
+	html1+="<span data-id='6' data-category='66' data-type='备注' data-name='备注' >备注</span>";
 	$(".DIY").html(html1);
 })
 
@@ -999,19 +1002,19 @@ $(".pwd_up").click(function(){
 	});
 
 function salType(type){
-	switch(type) {
-    case 1:
-     return  "收入"
-       break;
-    case 2:
-     return	"支出"
-       break;
-    case 4:
-        return	"专项附加扣除"
-          break;
-    default:
-    return  "汇总项";
-}
+		switch(type) {
+	    case 1:
+	     return  "收入"
+	       break;
+	    case 2:
+	     return	"支出"
+	       break;
+	    case 4:
+	        return	"专项附加扣除"
+	          break;
+	    default:
+	    return  "汇总项";
+	 }
 }
 
 
