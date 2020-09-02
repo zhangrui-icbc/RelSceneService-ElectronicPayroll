417f42ebdc68cb046a249ce550a35289e41a447f
tuwosh <434716122@qq.com>
2020-09-02 09:48:19 +0800

    优化后建表sql

---
+++ 20200902/reimbursement_import_new.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `reimbursement_import_new`;
+CREATE TABLE `reimbursement_import_new` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `batch_no` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '批次号',
+  `user_id` bigint(30) DEFAULT NULL COMMENT '员工编号(客户提供)/手机号码',
+  `issue_time` date DEFAULT NULL COMMENT '工资发放时间',
+  `totalReim` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '报销合计',
+  `special_info` text COLLATE utf8mb4_bin COMMENT '报销详细信息',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `company_id` varchar(30) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '公司id',
+  PRIMARY KEY (`id`,`create_time`),
+  KEY `companyid` (`company_id`),
+  KEY `userid` (`user_id`),
+  KEY `batchNo` (`batch_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='自定义数据表';
